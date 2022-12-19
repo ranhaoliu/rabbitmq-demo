5c37ec02beb82225177ae3ae169a576535e4ab85
ranhaoliu <1624351724@qq.com>
2022-12-19 17:23:30 +0800

    spring整合rabbimq高级特性,生产者 confirm和return消息
        当生产者发送的消息 不能到达交换机时(找不到)，回调confirm
        当生产者发送的消息 到达交换机，但不能到达队列时,回调return

---
+++ spring-rabbitmq-consumers-advanced/src/main/java/org/example/rabbitmq/listener/AckListener.java
@@ -0,0 +1,48 @@
+package org.example.rabbitmq.listener;
+
+
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AckListener implements ChannelAwareMessageListener {
+    @Override
+    public void onMessage(Message message, Channel channel) throws Exception {
+        //1.获取消息id
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        int count=0;
+       try{
+           //2.获取消息
+           System.out.println("message: "+new String(message.getBody()));
+           //3.进行业务处理
+           System.out.println("=====进行业务处理=======");
+           //模拟出现异常
+//           int i = 5/0;
+           //4.进行消息签收
+           channel.basicAck(deliveryTag,true);
+       }catch (Exception e){
+           //拒绝签收
+           /*第三个参数:requeue:重回队列。如果设置为true，则消息从新回到queue,broker会重新发送该消息给消费端,
+           如果为false则拒绝签收
+           * */
+           channel.basicNack(deliveryTag,true,true);
+//           channel.basicNack(deliveryTag,true,false);
+       }
+
+    }
+
+    @Override
+    public void onMessage(Message message) {
+
+    }
+
+    @Override
+    public void containerAckMode(AcknowledgeMode mode) {
+
+    }
+
+}
---
+++ spring-rabbitmq-consumers-advanced/src/test/java/org/example/Test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring-rabbitmq-consumer.xml");
+    }
+}
